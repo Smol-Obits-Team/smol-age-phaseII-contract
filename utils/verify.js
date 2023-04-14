5462af383846fb67c4f2eca619cfb74194154ae4
Ademola <aademolaoladipo@gmail.com>
2023-04-14 22:52:31 +0100

    update deployments scripts

--- utils/verify.js
+++ utils/verify.js
@@ -11,11 +11,3 @@ const verify = async (contractAddress, args) => {
     console.log(e);
   }
 };
-
-const addr = [
-  "0x598874F16084DD98Bbf41e7Eae609E634f1FeAfc",
-  "0x2d9AAB6330E8BF492fb3A78FD898207bDcFA9106",
-  "0xCA8408760bd4f0A7266b1aFf3cc3B3c7820C9B24",
-];
-
-for (let i = 0; i < addr.length; ++i) verify(addr[i], []);
